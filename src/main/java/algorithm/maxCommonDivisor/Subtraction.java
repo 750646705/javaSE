50569deda1704d86ed4f6f14a14d059aafdff89a
段旭强 <xuqiang_duan@trtjk.com>
2019-07-30 09:30:08 +0800

    求两数最大公约数——欧几里得算法（辗转相除法）&更相减损法

--- src/main/java/algorithm/maxCommonDivisor/Subtraction.java
+++ src/main/java/algorithm/maxCommonDivisor/Subtraction.java
@@ -1,45 +1,47 @@
 package algorithm.maxCommonDivisor;
 
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 /**
  * 求两个数的最大公约数
- * 欧几里得算法（又称辗转相除法）
+ * 更相减损法
  *
  * @author XUQIANG_DUAN
  * @date 2019/7/29
- * @time 9:21
+ * @time 10:24
  */
-public class EuclideanAlgorithm {
+public class Subtraction {
 
-    /**
-     * 辗转相除法
-     *
-     * @param a
-     * @param b
-     * @return 最大公约数
-     */
-    private static int euclidMethod(int a, int b) {
+    private static int subMethon(int a, int b) {
+
+        int result = 1;
         /**
          * 大值&小值
          */
         int maxI = a > b ? a : b;
         int minI = a > b ? b : a;
         /**
-         * 余数
+         * 大值&小值都为偶数，先简约
          */
-        int remainder = maxI % minI;
-//        System.out.println("余数为：" + remainder);
-        /**
-         * 余数为零，小值为最大公约数
-         * 余数为一，一为最大公约数
-         * 大值&小值的最大公约数与小值与余数的最大公约数相同
-         */
-        if (remainder == 0) {
-            return minI;
-        } else if (remainder == 1) {
-            return remainder;
-        } else {
-            return euclidMethod(minI, remainder);
+        while (maxI % 2 == 0 && minI % 2 == 0) {
+            result *= 2;
+            maxI /= 2;
+            minI /= 2;
         }
+        /**
+         * 差值
+         */
+        int sub = maxI - minI;
+//        System.out.println("差值为：" + sub + " | 大值为：" + maxI + " | 小值为：" + minI + " | 结果为：" + result);
+        if (sub == 0) {
+            result *= minI;
+        } else if (sub == 1) {
+            result *= 1;
+        } else {
+            result *= subMethon(minI, sub);
+        }
+
+        return result;
     }
 
     /**
@@ -56,22 +58,19 @@ public class EuclideanAlgorithm {
 
 //        long start = System.currentTimeMillis();
         long nstart = System.nanoTime();
-        System.out.println("(" + a + ", " + b + ") | result = " + euclidMethod(a, b));
+        System.out.println("(" + a + ", " + b + ") | result = " + subMethon(a, b));
 //        long end = System.currentTimeMillis();
         long nend = System.nanoTime();
 //        System.out.println("共耗时（单位：ms）：" + (end - start) + " | 结束点：" + end + " | 开始点：" + start);
         System.out.println("共耗时（单位：ns）：" + (nend - nstart) + " | 结束点：" + nend + " | 开始点：" + nstart);
-
     }
 
     public static void main(String[] args) {
-
-        timeConsume(55, 55);
-        timeConsume(66, 55);
-        timeConsume(10000, 10001);
-        timeConsume(10000, 10003);
+        timeConsume(5,55);
+        timeConsume(66,55);
+        timeConsume(10000,10001);
+        timeConsume(10000,10003);
         timeConsume(1350, 1266);
         timeConsume(1000000, 1000003);
-
     }
 }
